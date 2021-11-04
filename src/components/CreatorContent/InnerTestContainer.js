e8b406fdf47f878717733044914cb5a8f7277831
Timothy Crittenden <tim_critt@hotmail.com>
2021-11-04 03:41:34 +0100

    Reduced number of API calls when viewing  user content page. Tests now only load when container is expanded

---
+++ src/components/CreatorContent/InnerTestContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function InnerTestContainer({ tests }) {
+  return <div></div>;
+}
+
+export default InnerTestContainer;
--- src/components/CreatorContent/VerticallyExpandingTestsContainer.js
+++ src/components/CreatorContent/VerticallyExpandingTestsContainer.js
@@ -17,10 +17,27 @@ const VerticallyExpandingTestsContainer = ({
 }) => {
   const [testContainerExpanded, setTestContainerExpanded] = useState(false);
   const [tests, setTests] = useState(null);
-  const [hasFetched, setHasFetched] = useState(false);
+  const [filteredTests, setFilteredTests] = useState(null);
+  const [fetching, setFetching] = useState(false);
 
   const toggleExpandContainer = () => {
-    setTestContainerExpanded((prevState) => !prevState);
+    //only makes API call on first time container is expanded
+    if (!tests) {
+      const fetchTests = async () => {
+        setFetching(true);
+        let userTests = await getFilteredTests(creatorId, null, testType);
+        //stores a copy of ALL tests to allow for all tests to be displayed after removing filters without another API call
+        setTests(userTests);
+        //stores the tests to be displayed
+        setFilteredTests(userTests);
+        setFetching(false);
+        setTestContainerExpanded(true);
+      };
+
+      fetchTests();
+    } else {
+      setTestContainerExpanded((prevState) => !prevState);
+    }
   };
 
   //get user id from params
@@ -29,29 +46,29 @@ const VerticallyExpandingTestsContainer = ({
   //get tests and apply search filters
   useEffect(() => {
     var isMounted = true;
-    if (params) {
-      const fetchTests = async () => {
-        let userTests = await getFilteredTests(creatorId, null, testType);
+
+    if (params && tests) {
+      const filterTests = async () => {
         if (isMounted) {
-          var filteredTests = await JSON.parse(JSON.stringify(userTests));
+          var userTests = await JSON.parse(JSON.stringify(tests));
 
           //filter by topic tag
           if (tagFilterTerm) {
-            userTests = filteredTests.filter((doc) => doc.tags.includes(tagFilterTerm));
+            userTests = await userTests.filter((doc) => doc.tags.includes(tagFilterTerm));
           }
 
           //sort by date created
           if (sortBy === 'oldest') {
-            userTests = userTests.sort((a, b) => {
-              if (a.created >= b.created) {
+            userTests = await userTests.sort((a, b) => {
+              if (a.createdAt >= b.createdAt) {
                 return 1;
               } else {
                 return -1;
               }
             });
           } else if (sortBy === 'newest') {
-            userTests = userTests.sort((a, b) => {
-              if (a.created > b.created) {
+            userTests = await userTests.sort((a, b) => {
+              if (a.createdAt > b.createdAt) {
                 return -1;
               } else {
                 return 1;
@@ -60,58 +77,45 @@ const VerticallyExpandingTestsContainer = ({
           }
 
           //filter by question text
-          if (testType === 'FCEPart2' && questionFilterTerm && questionFilterTerm.length > 0) {
-            userTests = userTests.filter((test) =>
+          if (userTests.length > 0 && questionFilterTerm && questionFilterTerm.length > 0) {
+            userTests = await userTests.filter((test) =>
               test.questionOne.toUpperCase().includes(questionFilterTerm.toUpperCase())
             );
           }
-          setTests(userTests);
-          setHasFetched(true);
+          setFilteredTests(userTests);
         }
       };
-      fetchTests();
+      filterTests();
     }
     return () => {
       isMounted = false;
     };
-  }, [params, tagFilterTerm, creatorId, testType, sortBy, questionFilterTerm]);
+  }, [questionFilterTerm, tagFilterTerm, sortBy, params, tests]);
 
-  if (hasFetched) {
-    return (
-      tests.length > 0 && (
-        <Fragment>
-          <div
-            className={`tests-container-header ${
-              testContainerExpanded ? 'test-container-header-expanded' : ''
-            } `}
-          >
-            <div className='tests-container-heading' onClick={(e) => toggleExpandContainer(e)}>
-              <h2>
-                {buttonLabel}
-                <span className='number-of-tests'>{`(${tests.length})`}</span>
-              </h2>
-              <div className='tests-container-button'>
-                {testContainerExpanded ? <RemoveRoundedIcon /> : <ArrowDropDownIcon />}
-              </div>
-            </div>
-
-            <div
-              className={
-                'user-tests-container ' +
-                (testContainerExpanded ? 'user-tests-container-expanded' : '')
-              }
-            >
-              {cloneElement(children, { tests: tests })}
-            </div>
-          </div>
-        </Fragment>
-      )
-    );
-  }
   return (
-    <div className={'full-width'}>
-      <LinearProgress />
-    </div>
+    <Fragment>
+      <div
+        className={`tests-container-header ${
+          testContainerExpanded ? 'test-container-header-expanded' : ''
+        } `}
+      >
+        <div className='tests-container-heading' onClick={(e) => toggleExpandContainer(e)}>
+          <h2>{buttonLabel}</h2>
+          <div className='tests-container-button'>
+            {testContainerExpanded ? <RemoveRoundedIcon /> : <ArrowDropDownIcon />}
+          </div>
+        </div>
+        <div
+          className={
+            'user-tests-container ' + (testContainerExpanded ? 'user-tests-container-expanded' : '')
+          }
+        >
+          {cloneElement(children, { tests: filteredTests })}
+          {filteredTests && filteredTests.length === 0 && <div>no tests</div>}
+        </div>
+      </div>
+      {fetching && <LinearProgress />}
+    </Fragment>
   );
 };
 
