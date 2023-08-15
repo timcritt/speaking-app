79d1ea78b457824c10b5fb30f9b98d0d3914cdac
unknown <tim_critt@hotmail.com>
2023-08-15 22:24:52 +0200

    created custom text input component and added it to EditPart3

---
+++ src/components/TestCommon/ControlledFormInput.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ControlledFormInput = ({
+	label,
+	failedValidation,
+	required_input_incomplete,
+	placeholder,
+	onChange,
+	required,
+	textValue,
+}) => {
+	return (
+		<input
+			label={label}
+			className={`input question-input ${
+				failedValidation && required_input_incomplete
+			}`}
+			defaultValue={textValue}
+			placeholder={placeholder}
+			onChange={onChange}
+			required={required}
+		/>
+	);
+};
+
+export default ControlledFormInput;
