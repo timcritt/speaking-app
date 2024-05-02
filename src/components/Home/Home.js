4b8e58dc86b2be6e3ab1c949d1b4a48fe364de04
unknown <tim_critt@hotmail.com>
2024-05-02 23:26:58 +0200

    Logo and slogan on homepage now larger and styled

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,8 @@ const Home = () => {
 	return (
 		<div className={styles.home_content}>
 			<div className={styles.slogan_container}>
-				<h1 className={styles.slogan}>English oral exam material</h1>
+				<h1 className={styles.logo}> Orzilla</h1>
+				<h2 className={styles.slogan}>English oral exam material</h2>
 				<div className="home-btn-container"></div>
 			</div>
 		</div>
